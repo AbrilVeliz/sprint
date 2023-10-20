f7e6deae7ec06d5271656969a1d0d97b4e18f624
AbrilVeliz <abrilrociio@gmail.com>
2023-10-20 20:53:55 -0300

    commit de prueba

---
+++ login.js
@@ -0,0 +1,33 @@
+const $nombre = document.getElementById ("nombre");
+      $email = document.getElementById("email");
+      $contraseña = document.getElementById ("contraseña")
+      $fechanacimiento = document.getElementById("start");
+      $button = document.getElementById("button");
+      $form = document.getElementById ("form");
+//guardar inputs en variables
+const usuario = document.getElementById("nombre").value;
+const email = document.getElementById("email").value;
+const contraseña = document.getElementById("contraseña").value;
+      fechaNacimiento = document.getElementById("start").value;
+
+$form.addEventListener('submit', validarFormulario);
+$button.addEventListener("click", validarFormulario); //ahdiero mi boton de "iniciar sesion" para la sentencia de arriba
+
+function validarFormulario (evento) { //validarFormulario es la función que hace validar el form
+    
+    evento.preventDefault(); // Evita que el formulario se envíe automáticamente
+    
+    if($email.value.trim() === ""){
+        alert("Escribe tu email para iniciar sesión!")
+        return;
+    }if($contraseña.value.trim()=== ""){
+        alert("Por favor, escribe una contraseña")
+        return;
+    }if($nombre.value.trim()=== ""){
+        alert("Por favor, escribe tu nombre")
+        return;
+    }if($fechanacimiento.value.trim() >= "01/01/2015"){
+        alert("Debes ser mayor de edad")
+        return;
+        alert("Ingreso exitoso!")}}
+    
